87fee1bfe3ef6e4f100d3b0bd1e47de022ee9a3b
vogelba <vogelba@rose-hulman.edu>
2024-01-30 20:17:43 -0500

    Made Object class

--- src/mainApp/Object.java
+++ src/mainApp/Object.java
@@ -1,5 +1,38 @@
 package mainApp;
 
-public class Object {
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
 
+public abstract class Object {
+	private int x, y, constantSpeed;
+	private Color color;
+	private Shape shape;
+	
+	public Object(int x, int y, int constantSpeed, Color color, Shape shape)
+	{
+		this.x = x;
+		this.y = y;
+		this.constantSpeed = constantSpeed;
+		this.color = color;
+		this.shape = shape;
+	}
+	
+	public void drawOn(Graphics2D g)
+	{
+		g.setColor(this.color);
+		g.fill(shape);
+		g.setColor(Color.BLACK);
+		g.draw(shape);
+	}
+	
+	public void update()
+	{
+		x -= constantSpeed;
+		
+		if(x < 0)
+		{
+			System.out.println("Object is off screen");
+		}
+	}
 }
