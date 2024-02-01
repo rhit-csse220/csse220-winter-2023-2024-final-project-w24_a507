d86b6f01c26bafec7ea3363b0216227320535b88
rhit-thomastd <157434556+rhit-thomastd@users.noreply.github.com>
2024-02-01 11:45:49 -0500

    Enabled multiple levels
    Changed gameComp to switch to new level when old level ends

--- src/mainApp/gameComp.java
+++ src/mainApp/gameComp.java
@@ -11,85 +11,62 @@ import java.util.Random;
 
 import javax.swing.JComponent;
 
-
-
-
-
-public class gameComp extends JComponent{
-	Hero hero=new Hero();
+public class gameComp extends JComponent {
+	Hero hero = new Hero();
 	ArrayList<Object> objects = new ArrayList<Object>();
-	private int xForBarrier=400;
+	private int xForBarrier = 400;
 	Random random = new Random();
-	
-	public gameComp(){
-		
 
-		/*
-		objects.add(new Barrier(xForBarrier,random.nextInt(700),50,50,0));
-		
-		objects.add(new Barrier(xForBarrier,100,50,50,0));
-		
-		objects.add(new Barrier(xForBarrier,random.nextInt(700),50,50,0));
-		
-		objects.add(new Barrier(xForBarrier,random.nextInt(700),50,50,0));
+	private final int NUM_OF_LEVELS = 1;
 
-		objects.add(new ElectricBarrier(xForBarrier,random.nextInt(700),50,50,0,Color.pink));
-		
-		*/
-		
-		try {
-	        FileInputStream fileIn = new FileInputStream("level.ser");
-	        ObjectInputStream in = new ObjectInputStream(fileIn);
-	        ArrayList<Object> e = (ArrayList<Object>) in.readObject();
-	        objects = e;
-	        in.close();
-	        fileIn.close();
-	     } catch (IOException i) {
-	        i.printStackTrace();
-	     } catch (ClassNotFoundException c) {
-	        System.out.println("Employee class not found");
-	        c.printStackTrace();
-	     }
+	public gameComp() {
+
+		for (int x = 1; x <= NUM_OF_LEVELS; x++) {
+			try {
+				FileInputStream fileIn = new FileInputStream("level" + x + ".ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn);
+				ArrayList<Object> e = (ArrayList<Object>) in.readObject();
+				objects = e;
+				in.close();
+				fileIn.close();
+			} catch (IOException i) {
+				i.printStackTrace();
+			} catch (ClassNotFoundException c) {
+				System.out.println("Employee class not found");
+				c.printStackTrace();
+			}
+		}
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		
-		
-		
-		for(Object i : objects)
-		{
+
+		for (Object i : objects) {
 			i.drawOn(g2);
 		}
-	
-		
+
 		hero.drawOn(g2);
 	}
-	
-	public void update()
-	{
-		for (Object b: this.objects) {
+
+	public void update() {
+		for (Object b : this.objects) {
 			b.update();
-		}	
-		
-		for (Object b: this.objects) {
-			if (b.overlapsWith(hero) ) {
+		}
+
+		for (Object b : this.objects) {
+			if (b.overlapsWith(hero)) {
 				System.out.println("PLAYER WAS HIT");
-				
+
 			}
-	}
-		
-		
-		for(Object c: this.objects) {
-			if(c.overlapsWith(hero))
-			{
+		}
+
+		for (Object c : this.objects) {
+			if (c.overlapsWith(hero)) {
 				System.out.println("Coin was collected");
 			}
 		}
 	}
-	
-	
 
 }
